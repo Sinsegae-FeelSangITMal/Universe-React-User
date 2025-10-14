6f51e1c3251b0b61115434cc747f8e514a854666
min023dev <lovekmj23@gmail.com>
2025-10-14 21:50:04 +0900

    fix : 영상 송출 자막까지 에러 해결

--- vite.config.js
+++ vite.config.js
@@ -17,23 +17,17 @@ export default ({ mode }) => {
       port: 4444,
       open: true,
       proxy: {
-        '/api/live/subtitle': {
-          target: env.VITE_LIVE_URL,
-          changeOrigin: true,
-          proxyTimeout: 120000,
-          timeout: 120000,
-        },
         '/api': { target: env.VITE_API_URL, changeOrigin: true, proxyTimeout: 120000, timeout: 120000 },
         '/images': { target: env.VITE_API_URL, changeOrigin: true, proxyTimeout: 120000, timeout: 120000 },
 
         '/ws': { target: env.VITE_CHAT_URL, changeOrigin: true, ws: true, secure: false, proxyTimeout: 120000, timeout: 120000 },
         '/chatapi': { target: env.VITE_CHAT_URL, changeOrigin: true, proxyTimeout: 120000, timeout: 120000 },
 
-        // ★ 자막 WS (WebSocket)
+        // 자막 WS (WebSocket)
         '/ws-subtitle': {
           target: env.VITE_LIVE_URL,
           changeOrigin: true,
-          ws: true,              // ← 추가! WebSocket 업그레이드용
+          ws: true,
           secure: false,
           proxyTimeout: 120000,
           timeout: 120000,
